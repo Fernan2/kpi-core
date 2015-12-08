8ba1fd458adb60897e9c9329fd1068953c8d5750
Fernando Calatayud <fernandorankia@gmail.com>
2015-12-08 17:03:11 +0100

    conteo: create + (ugly) index

--- app/controllers/conteos_controller.rb
+++ app/controllers/conteos_controller.rb
@@ -1,9 +1,21 @@
 class ConteosController < ApplicationController
   def index
-    @conteos = Conteo.order(:fecha)
+    @conteos = Conteo.order(:dia)
   end
 
   def new
     @conteo = Conteo.new
+  end
+
+  def create
+    @conteo = Conteo.new(params_conteo)
+    @conteo.save or return render('new')
+    redirect_to conteos_path
+  end
+
+  private
+
+  def params_conteo
+    params.require(:conteo).permit(:dia, :loc_core, :loc_total)
   end
 end
